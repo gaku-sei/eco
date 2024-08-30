fb81e7d6f0f7002aef8afc657b7ef7924ae1f333
COMBRIAT Kévin <japan.combriat.kevin@gmail.com>
2024-08-30 12:40:29 +0900

    chore: Update Dioxus to 0.5.x

--- eco-view/src/components/doc_page.rs
+++ eco-view/src/components/doc_page.rs
@@ -1,28 +1,23 @@
 use dioxus::prelude::*;
 
-use crate::doc::{Doc, SharedDoc};
+use crate::FileType;
 
-#[allow(clippy::module_name_repetitions)]
-#[derive(Props)]
-pub struct DocPageProps<'a> {
-    doc: SharedDoc,
-    content: &'a str,
-}
+#[component]
+pub fn DocPage(content: ReadOnlySignal<String>) -> Element {
+    let file_type = use_context::<FileType>();
 
-pub fn DocPage<'a, 'b: 'a>(cx: Scope<'a, DocPageProps<'b>>) -> Element<'a> {
-    let content = cx.props.content;
-
-    match *cx.props.doc.lock().unwrap() {
-        Doc::Cbz { .. } => cx.render(rsx!(img {
+    match file_type {
+        FileType::Cbz => rsx!(img {
             class: "h-px grow",
             src: "data:image/png;base64,{content}"
-        })),
-        Doc::Epub { .. } => cx.render(rsx!(div {
-            class: "h-px grow spect-[12/16]",
-            iframe {
-                class: "h-full w-full",
-                src: "data:text/html;charset=utf-8,{content}"
+        }),
+        FileType::Epub => rsx!(
+            div { class: "h-px grow spect-[12/16]",
+                iframe {
+                    class: "h-full w-full",
+                    src: "data:text/html;charset=utf-8,{content}"
+                }
             }
-        })),
+        ),
     }
 }
